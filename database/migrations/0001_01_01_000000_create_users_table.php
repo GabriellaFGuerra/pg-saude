48640508d0b49634a4fb9d01c658b63855c8ce09
Gabriella Guerra <gabriellafguerra21@gmail.com>
2026-05-07 22:30:58 +0000

    create users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -15,6 +16,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('gender');
+            $table->date('birth_date');
+            $table->string('phone_number');
+            $table->string('mother')->nullable();
+            $table->string('father')->nullable();
+            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
